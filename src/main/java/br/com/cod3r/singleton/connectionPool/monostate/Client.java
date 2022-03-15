290c46cf26b618e378c6d6c7229411ef86f91b71
Breno Menezes <brenoma@live.com>
2022-03-14 23:12:30 -0300

    init repo

---
+++ src/main/java/br/com/cod3r/singleton/connectionPool/monostate/Client.java
@@ -0,0 +1,50 @@
+package br.com.cod3r.singleton.connectionPool.monostate;
+
+import br.com.cod3r.singleton.connectionPool.monostate.conn.Connection;
+import br.com.cod3r.singleton.connectionPool.monostate.conn.ConnectionPool;
+
+public class Client {
+	public static void doQuery1() {
+		ConnectionPool pool = new ConnectionPool();
+		Connection conn = pool.getConnection();
+		if(conn != null)
+			conn.query("SELECT * FROM A1");
+
+		pool.leaveConnection(conn);
+	}
+	
+	public static void doQuery2() {
+		ConnectionPool pool = new ConnectionPool();
+		Connection conn = pool.getConnection();
+		if(conn != null)
+			conn.query("SELECT * FROM A2");
+
+		pool.leaveConnection(conn);
+	}
+	
+	public static void doQuery3() {
+		ConnectionPool pool = new ConnectionPool();
+		Connection conn = pool.getConnection();
+		if(conn != null)
+			conn.query("SELECT * FROM A3");
+
+		pool.leaveConnection(conn);
+	}
+
+	public static void doQuery4() {
+		ConnectionPool pool = new ConnectionPool();
+		Connection conn = pool.getConnection();
+		if(conn != null)
+			conn.query("SELECT * FROM A4");
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		System.out.println("Connection Pool Size: " + ConnectionPool.POOL_SIZE);
+		int pool = ConnectionPool.POOL_SIZE;
+		System.out.println(pool);
+		doQuery1();
+		doQuery2();
+		doQuery3();
+		doQuery4();
+	}
+}
